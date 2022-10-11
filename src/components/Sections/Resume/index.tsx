b930d335a77222b291de5c9c56e3b65200323b25
Kumail Zaidi <kumail.zaidi@tufts.edu>
2022-10-10 21:03:52 -0400

    add sections

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,6 +1,6 @@
 import {FC, memo} from 'react';
 
-import {education, experience, SectionId, skills} from '../../../data/data';
+import {education, experience, teaching, SectionId, skills} from '../../../data/data';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
 import {SkillGroup} from './Skills';
@@ -17,6 +17,11 @@ const Resume: FC = memo(() => {
         </ResumeSection>
         <ResumeSection title="Leadership">
           {experience.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
+        </ResumeSection>
+        <ResumeSection title="Teaching">
+          {teaching.map((item, index) => (
             <TimelineItem item={item} key={`${item.title}-${index}`} />
           ))}
         </ResumeSection>
